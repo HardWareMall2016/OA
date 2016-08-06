a0a18ee4feee98d21c0db0c13ac1e43b48d08b40
伍岳 <wuyue8512@163.com>
2016-08-07 00:07:48 +0800

    WrapContentHeightViewPager

---
+++ app/src/main/java/com/android/wandong/ui/widget/FixViewPager.java
@@ -0,0 +1,62 @@
+package com.android.wandong.ui.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 作者：伍岳 on 2016/8/7 00:05
+ * 邮箱：wuyue8512@163.com
+ * //
+ * //         .............................................
+ * //                  美女坐镇                  BUG辟易
+ * //         .............................................
+ * //
+ * //                       .::::.
+ * //                     .::::::::.
+ * //                    :::::::::::
+ * //                 ..:::::::::::'
+ * //              '::::::::::::'
+ * //                .::::::::::
+ * //           '::::::::::::::..
+ * //                ..::::::::::::.
+ * //              ``::::::::::::::::
+ * //               ::::``:::::::::'        .:::.
+ * //              ::::'   ':::::'       .::::::::.
+ * //            .::::'      ::::     .:::::::'::::.
+ * //           .:::'       :::::  .:::::::::' ':::::.
+ * //          .::'        :::::.:::::::::'      ':::::.
+ * //         .::'         ::::::::::::::'         ``::::.
+ * //     ...:::           ::::::::::::'              ``::.
+ * //    ```` ':.          ':::::::::'                  ::::..
+ * //                       '.:::::'                    ':'````..
+ * //
+ */
+public class FixViewPager extends ViewPager {
+
+    public FixViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FixViewPager(Context context) {
+        super(context);
+    }
+    
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int height = 0;
+        //下面遍历所有child的高度
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if (h > height) //采用最大的view的高度。
+                height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
