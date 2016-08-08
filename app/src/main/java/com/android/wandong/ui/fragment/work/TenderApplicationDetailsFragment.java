cc9db06c594d84954ac02d11f1dbb59521c7eb15
keke <983198505@qq.com>
2016-08-08 17:42:22 +0800

    整合代码

--- app/src/main/java/com/android/wandong/ui/fragment/work/TenderApplicationDetailsFragment.java
+++ app/src/main/java/com/android/wandong/ui/fragment/work/TenderApplicationDetailsFragment.java
@@ -11,7 +11,6 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.android.wandong.R;
-import com.android.wandong.beans.MarketReimburseDetailResponseBean;
 import com.android.wandong.beans.TenderApplicationDetailsResponseBean;
 import com.android.wandong.network.ApiUrls;
 import com.android.wandong.ui.fragment.work.Tools.AuditStatusHelper;
